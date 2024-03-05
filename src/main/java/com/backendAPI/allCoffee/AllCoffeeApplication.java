996871d204a45d9f75e02b1fe83f8356d086fd1b
Aniket Kumar Singh <aniketks321@gmail.com>
2024-03-06 00:27:27 +0530

    Initial commit

---
+++ src/main/java/com/backendAPI/allCoffee/AllCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.backendAPI.allCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendAPI/allCoffee/AllCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendAPI.allCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
